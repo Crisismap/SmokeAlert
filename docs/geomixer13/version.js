4f079b7eb50552b15210631fb9c37b5fd5b9258a
OriginalSin <saleks@scanex.ru>
2018-04-05 10:18:13 +0300

    update

---
+++ docs/geomixer13/version.js
@@ -0,0 +1,8 @@
+var gmxVersion = {
+  "jsPath": {
+    "//www.kosmosnimki.ru/lib/geomixer/geomixer-src.js": 1522842969261
+  },
+  "cssPath": {
+    "//www.kosmosnimki.ru/lib/geomixer/geomixer.css": 1522842969261
+  }
+};
